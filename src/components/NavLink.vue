1515d9a58a3748f185ec05fdc5c92637f478e47c
Gabriel Pagotti <pagotti46@gmail.com>
2022-09-11 18:15:22 -0300

    Alterando o tamanho e espaçamento dos links na divisão da esquerda

--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="relative w-24 h-24 mb-6 m-auto cursor-pointer bg-black group last:w-28 last:h-28 last:mt-60">
+    <div class="relative w-20 h-20 mb-6 m-auto cursor-pointer bg-none bg-black group last:w-28 last:h-28 last:mt-60">
         <div class="absolute bg-white w-0 h-1.5 top-0 left-1/2 transition-all duration-500 group-hover:w-full group-hover:left-0"></div>
         <div class="absolute bg-white w-0 h-1.5 bottom-0 left-1/2 transition-all duration-500 group-hover:w-full group-hover:left-0"></div>
         <div class="absolute bg-white w-1.5 h-0 left-0 top-1/2 transition-all duration-500 group-hover:h-full group-hover:top-0"></div>
