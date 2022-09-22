0a4b38bce8e6cafe8d605cbf6a4c1faedc1423c4
Gabriel Pagotti <pagotti46@gmail.com>
2022-09-22 09:30:58 -0300

    Iniciando o desenvolvimento das oferendas

--- src/components/TopSection.vue
+++ src/components/TopSection.vue
@@ -4,9 +4,40 @@
 
 <template>
     <div class="row-span-2">
-        <h1 class="text-white">AAAAAAAAAAAAAAAAAAAAAA</h1>
+        <div>
+            <div class="hex"></div>
+            <div class="relative middle">
+                <img class="absolute w-[125px]" src="/images/icons/offerings/iconFavors_ardentRavenWreath_lg.png">
+            </div>
+            <div class="hex2"></div>
+        </div>
     </div>
 </template>
 
 <style scoped>
+    img {
+  margin: auto;
+  left: 0;
+  right: 0;
+  top: -38%;
+  text-align: center;
+  z-index: 1;
+    }
+    .hex {
+        width: 0;
+        border-bottom: 30px solid #000;
+        border-left: 52px solid transparent;
+        border-right: 52px solid transparent;
+    }
+    .middle {
+        width: 104px;
+        height: 60px;
+        background-color: #000;
+    }
+    .hex2 {
+        width: 0;
+        border-top: 30px solid #000;
+        border-left: 52px solid transparent;
+        border-right: 52px solid transparent;
+    }
 </style>
