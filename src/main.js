b068f2767a15d7fe7537b368c768afdc3a4076a7
Gabriel Pagotti <pagotti46@gmail.com>
2022-09-24 00:58:37 -0300

    Importando os componentes da primeira seção

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import SelectedPerks from './components/SelectedPerks.vue'
 import StuffList from './components/StuffList.vue'
 import TopSection from './components/TopSection.vue'
 import NavLink from './components/NavLink.vue'
+import Offering from './components/Offering.vue'
+import OfferingContainer from './components/OfferingContainer.vue'
+import PowerItem from './components/PowerItem.vue'
 
 import './assets/main.css'
 
@@ -20,5 +23,8 @@ app.component('LeftNavBar', LeftNavBar)
    .component('StuffList', StuffList)
    .component('TopSection', TopSection)
    .component('NavLink', NavLink)
+   .component('Offering', Offering)
+   .component('OfferingContainer', OfferingContainer)
+   .component('PowerItem', PowerItem)
 
 app.mount('#app')
