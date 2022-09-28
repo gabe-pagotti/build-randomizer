6774f171aadb76ffcfb13379efcbf6cae6ca9fad
Gabriel Pagotti <pagotti46@gmail.com>
2022-09-28 13:01:16 -0300

    Iniciando a randomização das oferendas

--- src/components/Offering.vue
+++ src/components/Offering.vue
@@ -1,5 +1,14 @@
 <script>
-
+export default {
+    props: {
+        "item": Object
+    },
+    data() {
+        return {}
+    },
+    methods: {
+    },
+}
 </script>
 
 <template>
@@ -11,7 +20,8 @@
         <div class="relative middle">
             <div class="absolute bg-white w-0.5 h-0 left-[-2px] bottom-0 transition-all duration-500 group-hover:h-[62px]"></div>
             <div class="absolute bg-white w-0.5 h-0 right-[-1px] bottom-[-1px] transition-all duration-500 group-hover:h-[62px]"></div>
-            <img class="absolute" src="/images/icons/offerings/iconFavors_ardentRavenWreath_lg.png">
+
+            <img class="absolute drop-shadow-2xl" :src="this.item.iconUrl">
         </div>
         <div class="hex2">
             <div class="absolute rotate-[30deg] w-0 bg-white h-0.5 transition-all duration-500 left-[-2px] bottom-[28px]  group-hover:w-[63px] group-hover:left-[-6px] group-hover:bottom-[13px]"></div>
--- src/components/OfferingContainer.vue
+++ src/components/OfferingContainer.vue
@@ -1,11 +1,37 @@
 <script>
+export default {
+    props: {
+    },
+    data() {
+        return {}
+    },
+    methods: {
+        getRadomOffering() {
+            let offerings = [
+                {
+                    name: 'Ardent Raven Wreath',
+                    description: 'Grants 100 % bonus Bloodpoints in the Deviousness Category.',
+                    rarity: 'Rare',
+                    iconUrl: "/images/icons/offerings/iconFavors_ardentRavenWreath_lg.png",
+                },
+                {
+                    name: 'Ivory Memento Mori',
+                    description: 'Grants the ability to kill one SurvivorIconHelpLoading survivor.png in the Dying State, who has progressed two Hook Stages',
+                    rarity: 'Rare',
+                    iconUrl: "/images/icons/offerings/iconFavors_momentoMoriIvory_lg.png",
+                }
+            ];
 
+            return offerings[Math.floor(Math.random() * offerings.length)];
+        }
+    },
+}
 </script>
 
 <template>
     <div class="flex flex-col gap-3 w-1/2 mt-auto align-bottom pl-10">
         <h1 class="text-white text-bold">OFFERING</h1>
-        <Offering />
+        <Offering :item="this.getRadomOffering()"/>
     </div>
 </template>
 
