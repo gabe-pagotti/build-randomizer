01ac3453b961cc09268f17a28a327926d46c12f6
Gabriel Pagotti <gabriel.pagotti@aurock.com.br>
2023-02-17 10:55:58 -0300

    Alterando o espaçamento da lista de offerings

--- src/components/Offering.vue
+++ src/components/Offering.vue
@@ -22,10 +22,12 @@ export default {
 </script>
 
 <template>
+    <div class="basis-1/6 grow-0">
         <div class="relative cursor-pointer bg-offering-rare bg-contain bg-no-repeat bg-center w-28 h-28" @click="select">
             <img src="/images/icons/offering-active.png" class="absolute" id="active" v-show="isSelected">
             <img id="icon" :src="this.offering.iconUrl">
         </div>
+    </div>
 </template>
 
 <style scoped>
